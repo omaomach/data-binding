48a5d4de70102dc10745737ef3f80b095ae88e47
omaomach <aaronjoash44@gmail.com>
2023-09-20 23:44:46 +0300

    worked on data binding

--- app/src/main/java/com/omao/databindingapp/MainActivity.kt
+++ app/src/main/java/com/omao/databindingapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.omao.databindingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.omao.databindingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.apply {
+            btn.setOnClickListener() {
+                var enteredText: String = editText.text.toString()
+                textView.setText("Hello $enteredText")
+            }
+        }
+
     }
 }
